0a89206a913c7c290039dace579d6ed196bfc233
TiGeorge <georgetikhomirov@mail.ru>
2020-04-09 21:45:19 +0300

    recipe

--- src/test/java/guru/springframework/services/RecipeServiceImplTest.java
+++ src/test/java/guru/springframework/services/RecipeServiceImplTest.java
@@ -22,8 +22,8 @@ public class RecipeServiceImplTest {
 
     @Before
     public void setUp() throws Exception {
-        MockitoAnnotations.initMocks(this);
-        recipeService = new RecipeServiceImpl(recipeRepository);
+//        MockitoAnnotations.initMocks(this);
+//        recipeService = new RecipeServiceImpl(recipeRepository);
     }
 
     @Test
